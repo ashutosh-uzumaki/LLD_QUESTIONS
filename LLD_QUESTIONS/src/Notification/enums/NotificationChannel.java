72088d1545405e9477145c1ee9c489f525817e41
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2026-04-24 22:12:53 +0530

    learnt book my show entities + uml

---
+++ LLD_QUESTIONS/src/Notification/enums/NotificationChannel.java
@@ -0,0 +1,7 @@
+package Notification.enums;
+
+public enum NotificationChannel {
+    EMAIL,
+    SMS,
+    PUSH
+}
---
+++ LLD_QUESTIONS/src/Notification/enums/NotificationType.java
@@ -0,0 +1,8 @@
+package Notification.enums;
+
+public enum NotificationType {
+    OTP,
+    ALERT,
+    ORDER_PLACED,
+    ORDER_SHIPPED
+}
